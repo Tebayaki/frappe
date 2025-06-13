29e6cea0c0577d1389f449219c3145d2895f79b8
Tebayaki <kongjiancong@qq.com>
2025-06-13 01:51:43 +0000

    feat: Chinese number system

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -1201,6 +1201,8 @@ Object.assign(frappe.utils, {
 			return number_systems.indian;
 		} else if (country == "Nepal") {
 			return number_systems.nepalese;
+		} else if (country == "China") {
+			return number_systems.china;
 		} else {
 			return number_systems.default;
 		}
